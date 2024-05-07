98d2d27e9ccd1b95245fb64570e27e36f1324f13
Dmitry Chubarin <chubarin@gmail.com>
2024-05-07 14:15:50 +0300

    initial commit

---
+++ AttackSolver/AttackCounter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace AttackSolver
+{
+    public enum ChessmanType
+    {
+        Rook, // ладья
+        Bishop // слон
+    }
+
+    public interface IAttackCounter
+    {
+        /// <summary>
+        /// Returns number of points which are under attack 
+        /// </summary>
+        /// <param name="boardSize"></param>
+        /// <param name="startCoords"></param>
+        /// <param name="obstacles"></param>
+        /// <returns></returns>
+        int CountUnderAttack(ChessmanType cmType, Size boardSize, Point startCoords, Point[] obstacles);
+    }
+
+    public class MyNaiveAttackCounter : IAttackCounter
+    {
+        public int CountUnderAttack(ChessmanType cmType, Size boardSize, Point startCoords, Point[] obstacles)
+        {
+            if (cmType == ChessmanType.Bishop)
+                return 3;
+            return 2;
+        }
+    }
+}
